6f4345b93cec5234788182b5a046cb57a8321441
Ilayoub <jrindou.ayoub@gmail.com>
2023-10-24 15:15:09 -0700

    0x1B. C - Sorting algorithms & Big O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * swap_ints - The function swap two integers in an array
+ * @x: The 1st integer to swap
+ * @y: The 2nd integer to swap
+ */
+void swap_ints(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * bubble_sort -  The function sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: The array of integers to sort
+ * @size: The size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
